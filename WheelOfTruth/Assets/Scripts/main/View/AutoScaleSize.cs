33c57807752672c8c68e0b44113c822fc3422f7f
Rakhan <mongvuot@gmail.com>
2016-04-08 00:53:24 +0700

    update

---
+++ WheelOfTruth/Assets/Scripts/main/View/AutoScaleSize.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScaleSize : MonoBehaviour
+{
+    private readonly float DEFAULT_RATIO = 16f / 9;
+	// Use this for initialization
+	void Start () 
+    {
+        float ratio = (float)Screen.height / Screen.width;
+
+        transform.localScale = Vector3.one * DEFAULT_RATIO / ratio;
+	}
+}
