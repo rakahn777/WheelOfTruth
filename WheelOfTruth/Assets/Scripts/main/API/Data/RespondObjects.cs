189eb6b570f3b0545b00e510d631fcdd9831e934
Rakhan <anhcv777@gmail.com>
2016-05-23 17:14:19 +0700

    update lib json + api

---
+++ WheelOfTruth/Assets/Scripts/main/API/Data/RespondObjects.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResLogin
+{
+    public string key;
+    public string user_id;
+
+}
--- WheelOfTruth/Assets/Scripts/main/API/RequestManager.cs
+++ WheelOfTruth/Assets/Scripts/main/API/RequestManager.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using strange.extensions.signal.impl;
 
 public class RequestManager : MonoBehaviour 
 {
+    public string SERVER_URL = "http://23.251.156.209/public/v1/";
+
+    //test
+    private string token;
+
     public void APIRegisterNewUser()
     {
-        string url = "http://23.251.156.209/public/v1/users";
+        string url = SERVER_URL + "users";
 
         WWWForm form = new WWWForm();
         form.AddField("email", "123@gmail.com");
@@ -18,13 +24,65 @@ public class RequestManager : MonoBehaviour
         StartCoroutine(WaitForRequest(www, null));
     }
 
-    private IEnumerator WaitForRequest(WWW www, EventDelegate callback)
+    public void APILoginEmail()
+    {
+        string url = SERVER_URL + "users/auth";
+
+        WWWForm form = new WWWForm();
+        form.AddField("email", "123@gmail.com");
+        form.AddField("password", "12345678");
+        form.AddField("device_id", SystemInfo.deviceUniqueIdentifier);
+        form.AddField("device_type", "Android");        
+        WWW www = new WWW(url, form);
+        
+        Debug.Log(LitJson.JsonMapper.ToJson(form));
+        
+        StartCoroutine(WaitForRequest(www, null));
+    }
+
+    public void APILogout()
+    {
+        string url = SERVER_URL + "users/1/logout";
+
+        WWWForm form = new WWWForm();
+        form.AddField("token", token);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www, null));
+    }
+
+    public void APIGetUserInfo()
+    {
+        string url = SERVER_URL + "users/1/show?token=";
+        url += token;
+        WWW www = new WWW(url);
+
+        StartCoroutine(WaitForRequest(www, null));
+    }
+
+    public void APIStartWheel()
+    {
+        string url = SERVER_URL + "wheels/start";
+
+        WWWForm form = new WWWForm();
+        form.AddField("token", token);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www, null));
+    }
+
+    private IEnumerator WaitForRequest(WWW www, Signal<string> signal)
     {
         yield return www;
 
         if (www.error == null)
         {
             Debug.Log("WWW Ok!: " + www.text);
+            ResLogin res = LitJson.JsonMapper.ToObject<ResLogin>(www.text);
+            Debug.Log(res.key);
+            token = res.key;
+
+            //signal.Dispatch(www.text);
         }
         else
         {
