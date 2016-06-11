e62b0587564f3f6838b1515e4cbe41b5213f3372
Rakhan <anhcv777@gmail.com>
2016-06-11 21:15:43 +0700

    update UI

---
+++ WheelOfTruth/Assets/Scripts/main/View/RoundView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundView : MonoBehaviour 
+{
+    public GameObject container;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
